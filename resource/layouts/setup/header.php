406383923e1240e532d80e22f98507599cd0fe88
Yuzlaks <yuzyus268@gmail.com>
2021-10-04 22:06:51 +0700

    update

--- resource/layouts/setup/header.php
+++ resource/layouts/setup/header.php
@@ -11,15 +11,13 @@
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="<?= asset('setup/css/styles.css') ?>" rel="stylesheet" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?php asset('plugins/fontawesome-free/css/all.min.css') ?>">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet"
-        href="<?php asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
+    <link rel="stylesheet" href="<?php asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
     <!-- iCheck -->
     <link rel="stylesheet" href="<?php asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
     <!-- JQVMap -->
@@ -68,19 +66,12 @@
         <div class="border-end bg-white" id="sidebar-wrapper">
             <div class="sidebar-heading border-bottom bg-light" style="font-size: 17px;">PandoraSetup</div>
             <div class="list-group list-group-flush layouts">
-                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku"
-                    href="database"><img src="<?= asset('setup/server.png') ?>" style="max-width:20px"> Database </a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku" href="table"><img
-                        src="<?= asset('setup/list.png') ?>" style="max-width:20px"> Table </a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku"
-                    href="controller"><img src="<?= asset('setup/controller.png') ?>" style="max-width:20px"> Controller
-                </a>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku" href="database"><img src="<?= asset('setup/server.png') ?>" style="max-width:20px"> Database </a>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku" href="table"><img src="<?= asset('setup/list.png') ?>" style="max-width:20px"> Table </a>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku" href="controller"><img src="<?= asset('setup/controller.png') ?>" style="max-width:20px"> Controller </a>
 
                 <span class="title-menu">Views</span>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku"
-                    data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
-                    aria-controls="collapseOne"><img src="<?= asset('setup/web-settings.png') ?>"
-                        style="max-width:20px"> Backend </a>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3 layoutku" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><img src="<?= asset('setup/web-settings.png') ?>" style="max-width:20px"> Backend </a>
                 <div id="accordion">
 
                     <div class="card">
@@ -91,25 +82,16 @@
                         }
 
                         ?>
-                        <div id="collapseOne" class="collapse <?= $class ?>" aria-labelledby="headingOne"
-                            data-parent="#accordion">
+                        <div id="collapseOne" class="collapse <?= $class ?>" aria-labelledby="headingOne" data-parent="#accordion">
                             <div class="card-body">
 
-                                <a href="backend-header"
-                                    class="list-group-item list-group-item-action list-group-item-light"><img
-                                        src="<?= asset('setup/header.png') ?>" style="max-width:20px"> Header </a>
+                                <a href="backend-header" class="list-group-item list-group-item-action list-group-item-light"><img src="<?= asset('setup/header.png') ?>" style="max-width:20px"> Header </a>
 
-                                <a href="backend-menu"
-                                    class="list-group-item list-group-item-action list-group-item-light "><img
-                                        src="<?= asset('setup/menu.png') ?>" style="max-width:20px"> Menu </a>
+                                <a href="backend-menu" class="list-group-item list-group-item-action list-group-item-light "><img src="<?= asset('setup/menu.png') ?>" style="max-width:20px"> Menu </a>
 
-                                <a href="backend-footer"
-                                    class="list-group-item list-group-item-action list-group-item-light"><img
-                                        src="<?= asset('setup/footer.png') ?>" style="max-width:20px"> Footer </a>
+                                <a href="backend-footer" class="list-group-item list-group-item-action list-group-item-light"><img src="<?= asset('setup/footer.png') ?>" style="max-width:20px"> Footer </a>
 
-                                <a href="backend-list-view"
-                                    class="list-group-item list-group-item-action list-group-item-light"><img
-                                        src="<?= asset('setup/list-view.png') ?>" style="max-width:20px"> List View </a>
+                                <a href="backend-list-view" class="list-group-item list-group-item-action list-group-item-light"><img src="<?= asset('setup/list-view.png') ?>" style="max-width:20px"> List View </a>
 
                             </div>
                         </div>
@@ -117,11 +99,7 @@
 
                 </div>
 
-                <a style="margin-top: -17px;"
-                    class="list-group-item list-group-item-action list-group-item-light p-3 layoutku"
-                    data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-                    aria-controls="collapseTwo"><img src="<?= asset('setup/front-end.png') ?>" style="max-width:20px">
-                    Frontend </a>
+                <a style="margin-top: -17px;" class="list-group-item list-group-item-action list-group-item-light p-3 layoutku" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"><img src="<?= asset('setup/front-end.png') ?>" style="max-width:20px"> Frontend </a>
                 <div id="accordionSecond">
 
                     <div class="card">
@@ -132,21 +110,14 @@
                         }
 
                         ?>
-                        <div id="collapseTwo" class="collapse <?= $classFrontend ?>" aria-labelledby="headingOne"
-                            data-parent="#accordionSecond">
+                        <div id="collapseTwo" class="collapse <?= $classFrontend ?>" aria-labelledby="headingOne" data-parent="#accordionSecond">
                             <div class="card-body">
 
-                                <a href="frontend-header"
-                                    class="list-group-item list-group-item-action list-group-item-light"><img
-                                        src="<?= asset('setup/header.png') ?>" style="max-width:20px"> Header </a>
+                                <a href="frontend-header" class="list-group-item list-group-item-action list-group-item-light"><img src="<?= asset('setup/header.png') ?>" style="max-width:20px"> Header </a>
 
-                                <a href="frontend-menu"
-                                    class="list-group-item list-group-item-action list-group-item-light"><img
-                                        src="<?= asset('setup/menu.png') ?>" style="max-width:20px"> Menu </a>
+                                <a href="frontend-menu" class="list-group-item list-group-item-action list-group-item-light"><img src="<?= asset('setup/menu.png') ?>" style="max-width:20px"> Menu </a>
 
-                                <a href="frontend-footer"
-                                    class="list-group-item list-group-item-action list-group-item-light"><img
-                                        src="<?= asset('setup/footer.png') ?>" style="max-width:20px"> Footer </a>
+                                <a href="frontend-footer" class="list-group-item list-group-item-action list-group-item-light"><img src="<?= asset('setup/footer.png') ?>" style="max-width:20px"> Footer </a>
 
                             </div>
                         </div>
@@ -160,50 +131,13 @@
             <!-- Top navigation-->
             <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                 <div class="container-fluid">
-                    <button class="btn btn-primary" id="sidebarToggle"> Sidebars </button>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation"><span
-                            class="navbar-toggler-icon"></span></button>
+                    <button class="btn btn-primary" id="sidebarToggle"> Sidebar </button>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-
-                            <?php
-                            
-                                if (shell_exec('git pull origin featured/yuz') !== "Already up to date.") {
-                            ?>
-                            <button type="button" class="btn btn-outline-primary btn-sm" data-toggle="modal"
-                                data-target="#exampleModal">
-                                Perbarui Versi
-                            </button>
-                            <?php } ?>
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                                aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Update PandoraSetup!
-                                                [Versi Terbaru]</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <?php
-                                                $check = shell_exec('git pull origin featured/yuz');
-
-                                                echo "<pre>";
-                                                    echo $check;
-                                                echo "</pre>";
-                                            ?>
-                                            <form action="" method="POST">
-                                                <button name="cek-update" class="btn btn-outline-primary btn-sm float-right">Update</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <form action="" method="POST">
+                                <button name="cek-update" class="btn btn-outline-primary btn-sm">Update versi</button>
+                            </form>
                         </ul>
                     </div>
                 </div>
@@ -217,10 +151,12 @@
 
                 function ExecuteShell()
                 {
+                    
+                    $check = shell_exec('git pull origin featured/yuz');
 
-                    shell_exec('git pull origin featured/yuz');
-                    alert('Berhasil Di Update');
-                    header("Refresh:0");
+                    alert('Berhasil Diperbarui');
+
+                    header('location:');
 
                 }
                 
