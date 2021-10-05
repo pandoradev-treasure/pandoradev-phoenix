f98648497c90ca28a20b6ca1a08a230f5d502177
Yuzlaks <yuzyus268@gmail.com>
2021-10-05 22:07:34 +0700

    up

--- core/app.php
+++ core/app.php
@@ -1,27 +1,17 @@
 <?php
 
-session_start();
+    session_start();
+    
 
+    @include 'helper.php';
+    @include 'database.php';
 
     error_reporting(0);
     ini_set('display_errors', 0);
 
-// error_reporting(0);
-// ini_set('display_errors', 0);
 
-
-if (strpos($_GET['params'], 'setup') !== false) {
-
-    $url           = $_GET['params'];
-    $url           = explode("/", $url);
-
-    $layoutsHeader = include "../resource/layouts/setup/header.php";
-    $getfile       = include "../resource/setup/$url[1].php";
-    $layoutsFooter = include "../resource/layouts/setup/footer.php";
-} else {
-    //auto redirect
-    if ($_GET['params']) {
-
+    if (strpos($_GET['params'],'setup') !== false) {
+        
         $url           = $_GET['params'];
         $url           = explode("/",$url);
 
