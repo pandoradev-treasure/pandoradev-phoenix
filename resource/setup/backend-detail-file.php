d03859c687f07c32f36fa5c2194d1d8645706942
Yuzlaks <yuzyus268@gmail.com>
2021-10-06 09:38:16 +0700

    updated rename folder in backend view detail

--- resource/setup/backend-detail-file.php
+++ resource/setup/backend-detail-file.php
@@ -28,44 +28,13 @@
                         } ?>
 
                     </div>
-                    <textarea class="mb-3 data-code-old" id="code-mirror"><?= file_get_contents('../resource/views/backend/'.$data->file[0]) ?></textarea>
+                    <textarea class="mb-3 data-code-old"
+                        id="code-mirror"><?= file_get_contents('../resource/views/backend/'.$data->file[0]) ?></textarea>
                     <div class="card-body">
                         <textarea style="display: none;" name="data_new_code" class="data-code" id="" cols="30"
                             rows="10"></textarea>
                     </div>
                 </form>
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Edit Nama Controller </h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <form action="<?= controller('setup@editNamaController') ?>" method="POST">
-                                <div class="modal-body">
-                                    <div class="form-inline">
-                                        <div class="form-group mb-2">
-                                            <input type="text" readonly class="form-control-plaintext" id="staticEmail2"
-                                                value="<?= $data->file[0] ?>">
-                                        </div>
-                                        <div class="form-group mx-sm-3 mb-2">
-                                            <label for="inputPassword2" class="sr-only">Nama Baru</label>
-                                            <input name="new_name_file" required type="text" class="form-control"
-                                                id="inputPassword2" placeholder="Nama Baru">
-                                        </div>
-                                        <input type="hidden" name="old_file" value="<?= $data->file[0] ?>">
-                                        <textarea style="display: none;" name="data_new_code" class="data-code" id=""
-                                            cols="30" rows="10"></textarea>
-                                        <button type="submit" class="btn btn-primary btn-sm mb-2">Ubah</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
