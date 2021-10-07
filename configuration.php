c2f6c42258d0d3762d01c7e876021b62cb3c47d9
Fajarsya <f4j4rkokok@gmail.com>
2021-10-07 23:45:59 +0700

    update docs

--- configuration.php
+++ configuration.php
@@ -1,11 +1,13 @@
 <?php
 
+
+
     $REDIRECT      = "";
     //$REDIRECT = UNTUK REDIRECT / PINDAH HALAMAN PADA SAAT PANDORACODE DIAKSES
 
     $HOST          = "";
     $USER          = "";
     $PASSWORD      = "";
-    $DATABASE      = "pandoracode_development";
+    $DATABASE      = "pandoracode_testing";
 
     #Jika Host, User, Password tidak diisi maka otomatis akan mengikuti settingan default XAMPP
