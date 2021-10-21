ba786db76d66cdab8197b9d0570d096b282116bd
Yuzlaks <yuzyus268@gmail.com>
2021-10-22 06:47:59 +0700

    update featured p-phoenix

--- resource/assets/backend/js/backend-main.js
+++
@@ -1,7 +0,0 @@
-$('.tambah-detail').click(function(){
-    var data  = "<tr>";
-	      data += "<td><input name='penerbit[]' class='form-control'><td>";
-	      data += "</tr>";
-		
-		$('tbody').append(data);
-});
